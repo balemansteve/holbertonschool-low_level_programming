84eae8796e0e48ee8307609fca4fe1ff751ca41a
balemansteve <balemansteve@gmail.com>
2024-02-16 12:54:49 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-/**Comment*/
-
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
-	{
-		puts("/"Programming is like building a multilingual puzzle");
-		return (0);
-	}
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
