4bdf12a0f12b6c5ef7dcc75ff6e67e7e5933627f
balemansteve <balemansteve@gmail.com>
2024-02-27 23:18:33 +0000

    puts_half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of a string.
+ *
+ * @str: string.
+ *
+ */
+void puts_half(char *str)
+{
+int len = _strlen(str);
+
+if (len % 2 == 0)
+len = len / 2;
+
+else
+len = (len + 1) / 2;
+
+while (str[len] != '\0')
+{
+_putchar(str[len]);
+len++;
+}
+_putchar('\n');
+}
+
+/**
+ * _strlen - return lenght of string.
+ * @s: string.
+ *
+ */
+int _strlen(char *s)
+{
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
+
+return (inc);
+}
