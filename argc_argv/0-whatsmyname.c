499d398f36b1acc9a0516bd99f25f3ca65efa43c
balemansteve <balemansteve@gmail.com>
2024-02-29 15:35:28 +0000

    mynameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - this program prints its name.
+ *
+ * @argv: string array that contain the arguments.
+ * @argc: number of arguments passed of program.
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
